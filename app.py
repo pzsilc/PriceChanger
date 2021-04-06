e03046ad98f92508f53c98add4d7b8d74fd2e0a4
Patryk PZ. Znamirowski <patryk.znamirowski@silcare.local>
2021-04-06 12:59:29 +0200

    Correct a twaeakly of export

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests, json, sys
 
 
 class Updater:
+    markers = 'ABCDEFGHIJKLMNOPQRST'
 
     def __init__(self):
         self.__input = self.__load_input() # input is products with prices from json (data.json must be inside the same location with .exe)
@@ -20,7 +21,7 @@ class Updater:
         }) #load prices list from static endpoint
         if res.status_code >= 400: 
             raise Exception('Server of Symfonia is not responsing currently. Might be unavailable right now. Try again later.')
-        return json.loads(res.text)[4:12]
+        return json.loads(res.text)
     
 
     def login(self): # login is using guid key to obtain a authorization, and then new session is opening
@@ -44,24 +45,32 @@ class Updater:
     
 
     def run(self): # essential engine of program
+        errors = list()
         static_prices = self.__load_prices() # load prices from endpoint
         for i in self.__input:
             code = i['Kod']
-            data = { "SalePrices": [] } # data for this instance is a dict which will be transfering to json as input data for product
-            prices = [k[1][:-3] for k in i.items()][2:] # extract price values from product's prices dictionary (2 first values in this case are ordinary names of product)
-            for index, price in enumerate(prices):
-                suitable_static_price = static_prices[index + 1 if index % 2 == 0 else index - 1] # adjust product prices to "static" prices from endpoint. In endpoint is reversed order of kinds of a same prices (i.e. brutto, netto -> netto, brutto). So we have to reverse each pair of values
-                data['SalePrices'].append({ # final instance of each price
-                    "Value": float(price), #value
+            total = { "SalePrices": [] } # data for this instance is a dict which will be transfering to json as input data for product
+            prices = [(key, float(i[key][:-3])) for key in i if key != 'Kod' and key != 'Nazwa']
+            for price in prices:
+                suitable_static_price = static_prices[Updater.markers.index(price[0])]
+                total['SalePrices'].append({ # final instance of each price
+                    "Value": price[1], #value
                     "PriceParameter": 2, #idk what is that but without this that had no worked
                     "Type": suitable_static_price['Type'], # required data from "static" prices
                     "Kind": suitable_static_price['Kind'] # as above
                 })
-            res = requests.put(f'{self.__host}/ProductPrices/Update?productCode={code}', data=json.dumps(data), headers={
+            res = requests.put(f'{self.__host}/ProductPrices/Update?productCode={code}', data=json.dumps(total), headers={
                 'Authorization': 'Session {' + self.__session + '}', 
                 'Content-Type': 'application/json'
             })
-            print(f"{code} updated" if res.status_code < 400 else f"{code} not updated")
+            if res.status_code < 400:
+                print(f"{code} updated")
+            else:
+                print(f"{code} not updated")
+                errors.append(i)
+        print("ERRORS WITH:")
+        for i in errors:
+            print(i['Kod'])
         return self
 
 
